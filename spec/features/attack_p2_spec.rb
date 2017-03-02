fb50ed5edd5c0e34bf7f1abc0a2e625101e21f0c
Ashwini <ashie.oh@gmail.com>
2017-03-02 12:38:07 +0000

    redid attack player 2 feature

--- spec/features/attack_p2_spec.rb
+++ spec/features/attack_p2_spec.rb
@@ -1,7 +1,7 @@
-feature 'Attacking' do
-  scenario 'attack Player 2' do
+feature "attack other player" do
+  it "attack player 2 and get confirmation" do
     sign_in_and_play
-    click_button 'Attack'
-    expect(page).to have_content 'Dave attacked Mittens'
+    click_button("attack")
+    expect(page).to have_content("Dave attacked Mittens")
   end
 end
