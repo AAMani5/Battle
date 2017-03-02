553929b629f11abedb4325b5169cae5543d31c4b
Ashwini <ashie.oh@gmail.com>
2017-03-02 13:27:16 +0000

    add model layer

--- battle.rb
+++ battle.rb
@@ -9,20 +9,20 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @name_1 = session[:name_1]
-    @name_2 = session[:name_2]
+    @name_1 = $name_1.name
+    @name_2 = $name_2.name
     erb(:play)
   end
 
   post '/names' do
-    session[:name_1] = params[:name_1]
-    session[:name_2] = params[:name_2]
+    $name_1 = Player.new(params[:name_1])
+    $name_2 = Player.new(params[:name_2])
     redirect '/play'
   end
 
   post '/attack' do
-    @name_1 = session[:name_1]
-    @name_2 = session[:name_2]
+    @name_1 = $name_1.name
+    @name_2 = $name_2.name
     erb(:attack)
   end
 
